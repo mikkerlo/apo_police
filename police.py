1f6ac97fa511ac7c73c57720c7456f8f02ae4580
mikkerlo <admin@mikkerlo.ru>
2018-01-26 11:38:36 +0300

    Add info logging

--- police.py
+++ police.py
@@ -7,7 +7,7 @@ import argparse
 
 def parse_arguments():
     parser = argparse.ArgumentParser(description="This program calling apo police for your list")
-    parser.add_argument('filename', help='Filename for apo police')
+    parser.add_argument('filename', help='filename for apo police')
     parser.add_argument('-v', '--verbosity', help='increase output verbosity', action='count')
     cli_args = parser.parse_args()
     return cli_args
@@ -25,8 +25,14 @@ def main():
     elif verbosity == 2:
         utils.setup_logging(logging.DEBUG)
 
+    logging.info('Whop-Whop')
+    logging.info('This is apo police siren!')
+    logging.info('Our team surrounds your list')
     temp_data = utils.read_from_file(filename)
+    logging.info('Environment completed successfully')
+    logging.info('Estimate a situation')
     names = mystem.process_names(temp_data)
+    logging.info('Today, without penalty, continue to be careful')
     print(*sorted(names), sep='\n')
 
 
